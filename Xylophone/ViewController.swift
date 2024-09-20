8d05f8756dc4700fc45bb44370c341452d82c32b
Vika <vg.dev31@gmail.com>
2024-09-20 12:10:25 +0400

    Xylophone with changing opacity of buttons

--- Xylophone/ViewController.swift
+++ Xylophone/ViewController.swift
@@ -7,15 +7,41 @@
 //
 
 import UIKit
+import AVFoundation
 
 class ViewController: UIViewController {
+    
+    var player: AVAudioPlayer!
 
     override func viewDidLoad() {
         super.viewDidLoad()
     }
 
-
+    @IBAction func keyPressed(_ sender: UIButton) {
+        
+            sender.alpha = 0.5
+            DispatchQueue.main.asyncAfter(deadline: .now() + 0.2) {
+                sender.alpha = 1.0
+            }
+        
+        playSound(soundName: sender.currentTitle!)
+//        transparencyOfButton(button: sender)
+    }
     
-
+    func playSound(soundName: String) {
+        let url = Bundle.main.url(forResource: soundName, withExtension: "wav")
+        player = try! AVAudioPlayer(contentsOf: url!)
+        player.play()
+                
+    }
+    
+// MARK:   альтернативный вариант
+    
+//    func transparencyOfButton(button: UIButton) {
+//        UIView.animate(withDuration: 0.2) {
+//            button.alpha = 0.5
+//            button.alpha = 1
+//        }
+//    }
+    
 }
-
